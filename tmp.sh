390577dd20761a46cf2268cc913160f24009c9f6
HYZ17 <yuzhenh17@gmail.com>
2026-02-02 18:00:15 +0800

    fix canvas env

---
+++ tmp.sh
@@ -0,0 +1,2 @@
+cd inference
+./run.sh --config-file debug.json --model deepseek-reasoner --max-context-size 130000 --base-url https://api.deepseek.com/v1 --api-key sk-aa13461dd1084c0a8730a1bed7bf4e6c
