dd0d157937b9e89e18b09e084c7e79814859be47
Yuzhen Huang <yuzhenh17@gmail.com>
2026-02-10 11:00:07 +0800

    Delete tmp.sh

--- tmp.sh
+++
@@ -1,2 +0,0 @@
-cd inference
-./run.sh --config-file debug.json --model deepseek-reasoner --max-context-size 130000 --base-url https://api.deepseek.com/v1 --api-key sk-aa13461dd1084c0a8730a1bed7bf4e6c
